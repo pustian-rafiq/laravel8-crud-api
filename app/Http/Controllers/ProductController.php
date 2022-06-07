e9e51c764a28425f0a4471a9732dda4f5a22c771
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-06-07 16:15:18 +0600

    product store api done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    //store product
+    public function store(Request $request){
+
+        $validateData = $request->validate([
+            'name' => 'required|max:200',
+            'desc' => 'required|max:200',
+            'price' => 'required|max:200',
+            'qty' => 'required|max:200',
+        ]);
+
+        $data = new Product();
+
+        $data->name  = $request->name;
+        $data->desc  = $request->desc;
+        $data->price = $request->price;
+        $data->qty   = $request->qty;
+
+        $data->save();
+
+        return response()->json([
+            'message' => "Product inserted successfully"
+        ],200);
+    }
 }
